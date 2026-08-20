021a200ceae2bbef0d987cc930736cb56cea5173
Pamel-C223171 <Mohammadpamel12345@gmail.com>
2026-08-20 11:55:18 +0300

    feat: add to the review feature and ui

--- app/(authGroup)/register/page.tsx
+++ app/(authGroup)/register/page.tsx
@@ -1,19 +1,19 @@
-// import RegisterForm from "../_components/RegisterForm";
+import RegisterForm from "../_components/RegisterForm";
 
-// export default function RegisterPage() {
-//   return (
-//     <div className="mx-auto max-w-md py-10">
-//       <div className="rounded-xl border bg-white p-6 shadow-sm">
-//         <h1 className="mb-2 text-3xl font-bold">
-//           Create Account
-//         </h1>
+export default function RegisterPage() {
+  return (
+    <div className="mx-auto max-w-md py-10">
+      <div className="rounded-xl border bg-white p-6 shadow-sm">
+        <h1 className="mb-2 text-3xl font-bold">
+          Create Account
+        </h1>
 
-//         <p className="mb-6 text-gray-500">
-//           Register for your RentNest account.
-//         </p>
+        <p className="mb-6 text-gray-500">
+          Register for your RentNest account.
+        </p>
 
-//         <RegisterForm />
-//       </div>
-//     </div>
-//   );
-// }
+        <RegisterForm />
+      </div>
+    </div>
+  );
+}
